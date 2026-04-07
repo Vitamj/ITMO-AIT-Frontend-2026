b748041f2f82c794a1bcd0b1aabba47988bb4d0f
Vitamj <140833068+Vitamj@users.noreply.github.com>
2026-04-07 20:43:33 +0300

    Добавляет HW3

--- assets/js/app.js
+++ assets/js/app.js
@@ -29,4 +29,23 @@ document.addEventListener('DOMContentLoaded', () => {
         default:
             break;
     }
+
+    const themeButton = document.getElementById('theme-toggle');
+    const savedTheme = localStorage.getItem('theme');
+
+    if (savedTheme === 'dark') {
+        document.body.classList.add('dark-theme');
+    }
+
+    if (themeButton) {
+        themeButton.addEventListener('click', () => {
+            document.body.classList.toggle('dark-theme');
+
+            if (document.body.classList.contains('dark-theme')) {
+                localStorage.setItem('theme', 'dark');
+            } else {
+                localStorage.setItem('theme', 'light');
+            }
+        });
+    }
 });
